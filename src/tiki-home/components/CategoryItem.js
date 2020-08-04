3c0b6458f7a514ed2f083e60743b49799a259792
Thien Nguyen <thiensmail96@gmail.com>
2020-08-04 21:40:22 +0700

    Improve CategoryItem

--- src/tiki-home/components/CategoryItem.js
+++ src/tiki-home/components/CategoryItem.js
@@ -1,6 +1,34 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, StyleSheet, Text} from 'react-native';
+import {TextSize, Padding} from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingHorizontal: Padding.base,
+    paddingVertical: Padding.base,
+    width: 92,
+    minHeight: 50,
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  text: {
+    fontSize: TextSize.xsmal,
+    padding: Padding.small,
+    textAlign: 'center',
+  },
+});
 
 export const CategoryItem = (props) => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{uri: props.image_url}} />
+      <Text style={styles.text} numberOfLines={2}>
+        {props?.title || ''}
+      </Text>
+    </View>
+  );
 };
